d6ae384f669bb5e9528be6569c9a7c5d060b1e3c
Dimitris <dimgkoutz@gmail.com>
2025-04-08 13:35:47 +0300

    adjust

--- src/ConfigurationGenerator.py
+++ src/ConfigurationGenerator.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 class ConfigGenerator:
-    def __init__(self, inner_radius=1.0, outer_radius=3.0):
+    def __init__(self, inner_radius=0.5, outer_radius=3.0):
         """
         Initialize the configuration generator for the robot.
 
@@ -15,18 +15,7 @@ class ConfigGenerator:
         self.outer_radius = outer_radius
 
     def generate_config(self, arm_pos_quat):
-        """
-        Generate a random robot configuration with the go2 spawned in an annulus.
-        
-        The annulus is defined by the inner_radius and outer_radius, centered at the arm's position.
-        
-        Parameters:
-            arm_pos_quat (iterable): The arm's position and quaternion (only x,y are used).
-            
-        Returns:
-            dict: A configuration dictionary that includes the robot's x, y, and yaw.
-                  Example: {"robot": {"x": 2.5, "y": 1.3, "yaw": -1.57}}
-        """
+       
         arm_pos_quat = np.array(arm_pos_quat)
         self.arm_position[0] = arm_pos_quat[0]
         self.arm_position[1] = arm_pos_quat[1]
@@ -34,6 +23,7 @@ class ConfigGenerator:
         # Sample a random radius uniformly in area between the inner and outer radius.
         r = np.sqrt(np.random.uniform(self.inner_radius**2, self.outer_radius**2))
         theta = np.random.uniform(0, 2 * np.pi)
+        print("r",r)
 
         pos_x = self.arm_position[0] + r * np.cos(theta)
         pos_y = self.arm_position[1] + r * np.sin(theta)
