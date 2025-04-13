21da6b29304e338c9ec4dcee1b2508fcefb1e1b5
Dimitris <dimgkoutz@gmail.com>
2025-04-13 19:18:47 +0300

    bin attempt

--- src/move_arm.py
+++ src/move_arm.py
@@ -53,8 +53,8 @@ mujoco.mjv_defaultCamera(cam)
 cam.type = mujoco.mjtCamera.mjCAMERA_FREE
 mujoco.mjv_defaultOption(opt)
 
-cam.azimuth, cam.elevation, cam.distance = -90.48, -40.10, 3.21
-cam.lookat[:] = [2.5, -1, 0.3]
+cam.azimuth, cam.elevation, cam.distance = -90.48, -40.10, 5
+cam.lookat[:] = [0.0, 2, 0.5]
 
 # ========== Mouse Handling ==========
 mouse = {"left": False, "middle": False, "right": False, "last_x": 0.0, "last_y": 0.0}
@@ -98,10 +98,10 @@ glfw.set_scroll_callback(window, scroll_callback)
 # Update arm pos
 mujoco.mj_forward(model,data)
 
-arm_pos_quat, _ = arm.get_CoM_pos(data)
-init_config = generator.generate_config(arm_pos_quat=arm_pos_quat)
+# arm_pos_quat, _ = arm.get_CoM_pos(data)
+# init_config = generator.generate_config(arm_pos_quat=arm_pos_quat)
 
-robot_go2.set_CoM_pos(data,config=init_config)
+# robot_go2.set_CoM_pos(data,config=init_config)
 
 
 # mujoco.mj_forward(model,data)
@@ -124,16 +124,14 @@ def my_controller(model, data):
     if (data.time - t_last > 0.034): #fps(30)
         t_last = data.time
     
+ 
         rgb, _ = perception._render_camera_view(model, data, perception.perception_context)
 
-        detections = yolo.detect(rgb)
 
-        for label, conf, (x1, y1, x2, y2) in detections:
-            cv2.rectangle(perception.rgb, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv2.putText(perception.rgb, f"{label} {conf:.2f}", (x1, y1 - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-        cv2.imshow("YOLO Detection", perception.rgb)
-        
+        # Run detection
+        predicted , _ = yolo.detect(frame=rgb)
+                
+        print("Predicted: ", predicted)
         perception.Cb_DnT(frame=rgb)
 
     #Print joint mapping
--- src/yolo_detector.py
+++ src/yolo_detector.py
@@ -1,4 +1,3 @@
-# yolo_detector.py
 from ultralytics import YOLO
 import cv2
 
@@ -6,12 +5,12 @@ class YOLODetector:
     def __init__(self, model_path="yolo11n.pt"): 
         self.model = YOLO(model_path)
 
-    def detect(self, frame_bgr):
+    def detect(self, frame):
         """
         Detect objects in a given BGR frame.
-        Returns a list of (label, confidence, (x1, y1, x2, y2)) tuples.
+        Returns: List of (label, confidence, (x1, y1, x2, y2), area)
         """
-        results = self.model.predict(source=frame_bgr, stream=True, verbose=False)
+        results = self.model.predict(source=frame, stream=True, verbose=False, conf=0.25, device="0")
         detections = []
 
         for result in results:
@@ -20,7 +19,12 @@ class YOLODetector:
                 label = result.names[cls_id]
                 conf = float(box.conf.item())
                 x1, y1, x2, y2 = map(int, box.xyxy[0].tolist())
-                detections.append((label, conf, (x1, y1, x2, y2)))
-
-        return detections
-
+                area = (x2 - x1) * (y2 - y1)
+                
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                cv2.putText(frame, f"{label} {conf:.2f}", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+                
+                detections.append((label, conf, (x1, y1, x2, y2), area))
+                
+                
+        return detections, frame
