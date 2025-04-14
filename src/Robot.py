4c46acca1012b9e21ab2c65c3c948506c83ee22c
DIMITRIOS GKOUTZOUNIS <58829316+DimitrisGkoutzounis@users.noreply.github.com>
2025-04-14 23:21:15 +0300

    update robot info usin get_coM_pos

--- src/Robot.py
+++ src/Robot.py
@@ -68,4 +68,7 @@ class RobotGo2:
         
         data.qpos[self.i_base_start_qpos:self.i_base_end_qpos] = new_base_state
         
+        #update the pc and xquat
+        self.get_CoM_pos(self,data)
+        
         # print("RobotGo2: set base qpos:", data.qpos[self.i_base_start_qpos:self.i_base_end_qpos])
